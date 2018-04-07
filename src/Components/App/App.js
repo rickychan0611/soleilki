235242d30dac43ea0ed4b74788adf5575b8e128e
iamrickychan <ric0611@gmail.com>
2018-04-07 16:29:16 -0700

    set up baic app.js react component

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,17 +1,16 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import {menuList} from '../menuList/menuList';
 
-export class App extends Component {
+export class App extends React.Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          Restaurant ordering system by Ricky Chan.
         </p>
       </div>
     );
